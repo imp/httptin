111e1784335f3326a847c566b25b5f64fba74d0e
Cyril Plisko <cyril.plisko@mountall.com>
2017-01-04 18:00:20 +0200

    Simple dispatch on GET method

--- src/get.rs
+++ src/get.rs
@@ -1,39 +1,79 @@
+use std::io::Write;
+use hyper::header::{ContentLength, ContentType};
 use hyper::server::{Request, Response};
+use hyper::status::StatusCode;
+use hyper::uri::RequestUri;
 
-pub fn get(request: Request, mut response: Response) {
-    let (remote_addr, _, headers, uri, version, payload) = request.deconstruct();
-    println!("**  Handling GET {}", uri);
-    println!("*** Incoming headers {:?}", headers);
+macro_rules! dispatch {
+    ($m0:expr => $h0:expr, $($m1:expr => $h1:expr,)*) => {{
+        if $m0 { $h0 }
+        $( else if $m1 { $h1 } )*
+    }}
 }
 
-// // #[get("/")]
-// fn index() -> content::HTML<&'static str> {
-//     content::HTML("<!DOCTYPE html>
-//     <html>
-//         <head>
-//             <title>HTTPTIN</title>
-//         </head>
-//         <body>
-//         <h1>HTTPTIN - HTTP tester in Rust and Rocket</h1>
-//         </body>
-//     </html>")
-// }
-//
-// // #[get("/test")]
-// fn test(data: IndexData) -> content::HTML<String> {
-//     content::HTML(format!("<!DOCTYPE html>
-//     <html>
-//         <head>
-//             <title>HTTPTIN></title>
-//         </head>
-//         <body>{}</body>
-//     </html>",
-//                           data))
-// }
+pub fn get(request: Request, mut response: Response) {
+    // let (remote_addr, _, headers, uri, version, payload) = request.deconstruct();
+    println!("** Handling GET {}", request.uri);
+    println!("** Incoming headers {:?}", request.headers);
+    if let RequestUri::AbsolutePath(ref path) = request.uri {
+        dispatch![
+            path == "/" => index(&request, response),
+            path == "/ip" => origin(&request, response),
+            path.starts_with("/status") => status(&request, response),
+            path.starts_with("/test") => test(&request, response),
+        ];
+    }
+}
 
-//#[get("/ip")]
-fn origin() -> String {
-    String::from("Remote address decoding is not implemented yet")
+fn index(request: &Request, mut response: Response) {
+    let index = "<!DOCTYPE html>
+    <html>
+        <head>
+            <title>HTTPTIN</title>
+        </head>
+        <body>
+        <h1>HTTPTIN - HTTP tester in Rust and Rocket</h1>
+        </body>
+    </html>";
+
+    response.headers_mut().set(ContentType::html());
+    response.headers_mut().set(ContentLength(index.len() as u64));
+    response.start().unwrap().write(index.as_bytes());
+}
+
+fn status(request: &Request, mut response: Response) {
+    *response.status_mut() = StatusCode::ImATeapot;
+}
+
+fn origin(request: &Request, mut response: Response) {
+    let text = "Remote address decoding is not implemented yet";
+    response.headers_mut().set(ContentType::plaintext());
+    response.headers_mut().set(ContentLength(text.len() as u64));
+    response.start().unwrap().write(text.as_bytes());
+}
+
+fn test(request: &Request, mut response: Response) {
+    let text = format!("<!DOCTYPE html>
+    <html>
+        <head>
+            <title>HTTPTIN TEST</title>
+        </head>
+        <body>
+            Remote Address: {}<br>
+            Method: {}<br>
+            HTTPVersion: {}<br>
+            Headers: {}<br>
+            URI: {}<br>
+        </body>
+    </html>",
+                       request.remote_addr,
+                       request.method,
+                       request.version,
+                       request.headers,
+                       request.uri);
+    response.headers_mut().set(ContentType::html());
+    response.headers_mut().set(ContentLength(text.len() as u64));
+    response.start().unwrap().write(text.as_bytes());
 }
 
 struct GetData {
