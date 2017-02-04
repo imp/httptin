8d0f04ce31ad2a3bb6e6e9ad6964eeb71c8a2083
Cyril Plisko <cyril.plisko@mountall.com>
2017-02-04 20:39:10 +0200

    Translate Headers into HashMap explicitly
    Don't use hyper_serde

--- src/get/headers.rs
+++ src/get/headers.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use hyper::header::{ContentType, Headers};
 use hyper::server::{Request, Response};
 use hyper_serde::serialize_pretty;
@@ -6,9 +8,27 @@ use serde_json::to_string_pretty;
 use makeresponse::MakeResponse;
 
 #[derive(Serialize)]
-pub struct HeadersData {
+pub struct NativeHeadersData {
     #[serde(serialize_with="serialize_pretty")]
     headers: Headers,
+}
+
+impl MakeResponse for NativeHeadersData {
+    fn content_type(&self) -> ContentType {
+        ContentType::json()
+    }
+
+    fn make_response(&self, mut response: Response) {
+        *response.status_mut() = self.status();
+        response.headers_mut().set(self.content_type());
+        let body = to_string_pretty(self).unwrap_or_else(|_| String::new());
+        response.send(body.as_bytes()).unwrap();
+    }
+}
+
+#[derive(Serialize)]
+pub struct HeadersData {
+    headers: HashMap<String, String>,
 }
 
 impl MakeResponse for HeadersData {
@@ -25,5 +45,10 @@ impl MakeResponse for HeadersData {
 }
 
 pub fn headers(request: &Request) -> HeadersData {
-    HeadersData { headers: request.headers.clone() }
+    // NativeHeadersData { headers: request.headers.clone() }
+    let headers = request.headers
+        .iter()
+        .map(|h| (h.name().to_string(), h.value_string()))
+        .collect::<HashMap<_, _>>();
+    HeadersData { headers: headers }
 }
