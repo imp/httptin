49a05e58da00399cddd078365dcdd27bb6ff6543
Cyril Plisko <cyril.plisko@mountall.com>
2017-02-07 17:08:12 +0200

    Keep index.html as a separate file

--- src/get/index.rs
+++ src/get/index.rs
@@ -1,15 +1,6 @@
 use makeresponse::Html;
 
-const INDEX: &'static str =
-"<!DOCTYPE html>
-<html>
-    <head>
-        <title>HTTPTIN</title>
-    </head>
-    <body>
-    <h1>HTTPTIN - HTTP tester in Rust and Rocket</h1>
-    </body>
-</html>";
+const INDEX: &'static str = include_str!("index.html");
 
 pub fn index() -> Html {
     Html(String::from(INDEX))
